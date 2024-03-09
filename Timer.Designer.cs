3b977263f8fdd346435ebbc1feb95ac3e041c4b8
Hamit Can SAYILGAN <hamitcansayilgan@gmail.com>
2024-03-10 00:42:00 +0300

    Add project files.

---
+++ Timer.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Timer
+{
+	partial class Timer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.btnStop = new System.Windows.Forms.Button();
+			this.lblTime = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.btnUpdate = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnStart
+			// 
+			this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnStart.Location = new System.Drawing.Point(42, 120);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(104, 52);
+			this.btnStart.TabIndex = 0;
+			this.btnStart.Text = "START";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+			// 
+			// btnStop
+			// 
+			this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnStop.Location = new System.Drawing.Point(152, 120);
+			this.btnStop.Name = "btnStop";
+			this.btnStop.Size = new System.Drawing.Size(104, 52);
+			this.btnStop.TabIndex = 1;
+			this.btnStop.Text = "STOP";
+			this.btnStop.UseVisualStyleBackColor = true;
+			this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+			// 
+			// lblTime
+			// 
+			this.lblTime.AutoSize = true;
+			this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblTime.Location = new System.Drawing.Point(37, 62);
+			this.lblTime.Name = "lblTime";
+			this.lblTime.Size = new System.Drawing.Size(93, 39);
+			this.lblTime.TabIndex = 2;
+			this.lblTime.Text = "Time";
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// btnUpdate
+			// 
+			this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnUpdate.Location = new System.Drawing.Point(314, 239);
+			this.btnUpdate.Name = "btnUpdate";
+			this.btnUpdate.Size = new System.Drawing.Size(106, 28);
+			this.btnUpdate.TabIndex = 3;
+			this.btnUpdate.Text = "UPDATE";
+			this.btnUpdate.UseVisualStyleBackColor = true;
+			this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(314, 213);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(104, 20);
+			this.textBox1.TabIndex = 4;
+			// 
+			// Timer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(466, 279);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.btnUpdate);
+			this.Controls.Add(this.lblTime);
+			this.Controls.Add(this.btnStop);
+			this.Controls.Add(this.btnStart);
+			this.Name = "Timer";
+			this.Text = "Timer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.Button btnStop;
+		private System.Windows.Forms.Label lblTime;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button btnUpdate;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
---
+++ Timer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Timer
+{
+	public partial class Timer : Form
+	{
+		public Timer()
+		{
+			InitializeComponent();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			lblTime.Text = DateTime.Now.ToString();
+		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			timer1.Start();
+		}
+
+		private void btnStop_Click(object sender, EventArgs e)
+		{
+			timer1.Stop();
+		}
+
+		private void btnUpdate_Click(object sender, EventArgs e)
+		{
+			timer1.Interval = Convert.ToInt32(textBox1.Text) * 1000;
+		}
+
+		
+
+	}
+}
